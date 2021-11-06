85fb68ce5799e9497db461503bae35743aae929d
bdimitrow <bozhidar.dimitrow@gmail.com>
2021-11-06 11:24:00 +0200

    initial files

--- lab04/src/bg/sofia/uni/fmi/mjt/investment/wallet/InvestmentWallet.java
+++ lab04/src/bg/sofia/uni/fmi/mjt/investment/wallet/InvestmentWallet.java
@@ -1,4 +1,65 @@
 package bg.sofia.uni.fmi.mjt.investment.wallet;
 
-public class InvestmentWallet {
+import bg.sofia.uni.fmi.mjt.investment.wallet.acquisition.Acquisition;
+import bg.sofia.uni.fmi.mjt.investment.wallet.asset.Asset;
+import bg.sofia.uni.fmi.mjt.investment.wallet.exception.InsufficientResourcesException;
+import bg.sofia.uni.fmi.mjt.investment.wallet.exception.UnknownAssetException;
+import bg.sofia.uni.fmi.mjt.investment.wallet.exception.WalletException;
+import bg.sofia.uni.fmi.mjt.investment.wallet.quote.QuoteService;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class InvestmentWallet implements Wallet{
+    private QuoteService quoteService;
+
+    public InvestmentWallet(QuoteService quoteService){
+        this.quoteService = quoteService;
+    }
+
+
+    @Override
+    public double deposit(double cash) {
+        return 0;
+    }
+
+    @Override
+    public double withdraw(double cash) throws InsufficientResourcesException {
+        return 0;
+    }
+
+    @Override
+    public Acquisition buy(Asset asset, int quantity, double maxPrice) throws WalletException {
+        return null;
+    }
+
+    @Override
+    public double sell(Asset asset, int quantity, double minPrice) throws WalletException {
+        return 0;
+    }
+
+    @Override
+    public double getValuation() {
+        return 0;
+    }
+
+    @Override
+    public double getValuation(Asset asset) throws UnknownAssetException {
+        return 0;
+    }
+
+    @Override
+    public Asset getMostValuableAsset() {
+        return null;
+    }
+
+    @Override
+    public Collection<Acquisition> getAllAcquisitions() {
+        return null;
+    }
+
+    @Override
+    public Set<Acquisition> getLastNAcquisitions(int n) {
+        return null;
+    }
 }
---
+++ lab04/src/bg/sofia/uni/fmi/mjt/investment/wallet/asset/Gold.java
@@ -0,0 +1,4 @@
+package bg.sofia.uni.fmi.mjt.investment.wallet.asset;
+
+public class Gold {
+}
